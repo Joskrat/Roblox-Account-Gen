0deb22a90282998634cc295637697eadf3b94bad
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2021-11-05 21:53:37 +0100

    Update loginModule.py

--- util/loginModule.py
+++ util/loginModule.py
@@ -1,77 +1,64 @@
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import Select
-from selenium import webdriver
+from util.passwordGen import passwordGen
+from util.usernameGen import nameGen
+from util.loginModule import login
 from colorama import Fore
-import random
-import time
+import threading
+import requests
+import zipfile
+import wget
+import os
 
-
-months = ["Januar", "Februar", "März", "April", "Mai", "Juni",
-          "Juli", "August", "September", "Oktober", "November", "Dezember"]
 r = Fore.RED
 g = Fore.GREEN
 b = Fore.BLUE
 c = Fore.CYAN
-m = Fore.LIGHTMAGENTA_EX
+ml = Fore.LIGHTMAGENTA_EX
+m = Fore.MAGENTA
 y = Fore.LIGHTYELLOW_EX
 w = Fore.WHITE
 
 
-def login(username: str, password: str):
-    options = Options()
-    options.headless = True
-    options.add_experimental_option("excludeSwitches", ["enable-logging"])
-    bot = webdriver.Chrome(chrome_options=options)
-    bot.get("https://roblox.com")
-    time.sleep(3)
+def download_chromedriver():
+    url = 'https://chromedriver.storage.googleapis.com/LATEST_RELEASE'
+    response = requests.get(url)
+    version_number = response.text
 
-    if len(username) > 20:
-        username = str(username[:20])
+    download_url = "https://chromedriver.storage.googleapis.com/" + \
+        version_number + "/chromedriver_win32.zip"
 
-    day = Select(bot.find_element_by_xpath('//*[@id="DayDropdown"]'))
-    month = Select(bot.find_element_by_xpath('//*[@id="MonthDropdown"]'))
-    year = Select(bot.find_element_by_xpath('//*[@id="YearDropdown"]'))
-    usernameInput = bot.find_element_by_xpath('//*[@id="signup-username"]')
-    passwordInput = bot.find_element_by_xpath('//*[@id="signup-password"]')
-    register = bot.find_element_by_xpath('//*[@id="signup-button"]')
-    accept = bot.find_element_by_xpath(
-        '//*[@id="cookie-banner-wrapper"]/div[1]/div[2]/div/div/button[2]')
+    latest_driver_zip = wget.download(download_url, 'chromedriver.zip')
 
-    # Dob - Day Selector
-    dayVal = random.randint(1, 27)
-    if dayVal < 10:
-        dayVal = "0" + str(dayVal)
-    day.select_by_value(str(dayVal))
-    # time.sleep(1)
+    with zipfile.ZipFile(latest_driver_zip, 'r') as zip_ref:
+        zip_ref.extractall()
+    os.remove(latest_driver_zip)
 
-    # Dob - Month Selector
-    monthVal = random.choice(months)
-    month.select_by_visible_text(str(monthVal))
-    # time.sleep(1)
 
-    # Dob - Year Selector
-    yearVal = random.randint(1960, 2010)
-    year.select_by_visible_text(str(yearVal))
-    # time.sleep(1)
+def gen():
+    username = nameGen()
+    password = passwordGen()
+    login(str(username), str(password))
 
-    # Username
-    usernameInput.clear()
-    usernameInput.send_keys(username)
 
-    # Password
-    passwordInput.clear()
-    passwordInput.send_keys(password)
+threads = []
+if __name__ == "__main__":
+    os.system('mode 110,25')
+    os.system("cls;clear")
+    os.system(
+        "title Roblox Account Gen ^|    Idle    ^| Made by TerrificTable55™#5297")
+    threadsInput = input(f"[{m}>{w}] Amount of threads: {c}")
 
-    # Register
-    try:
-        accept.click()
-        time.sleep(1)
-        register.click()
-        # Log
-        print(f"[{g}Username{w}] {y}{username}{w} \n[{b}Password{w}] {y}{password}{w}\n[{c}Day{w}]      {m}{dayVal}{w} \n[{c}Month{w}]    {m}{monthVal}{w} \n[{c}Year{w}]     {m}{yearVal}{w}\n\n")
-        with open("./logins.txt", "a") as f:
-            f.write(f"[Username] {username} \n[Password] {password}\n\n\n")
-        bot.close()
-    except:
-        print("ERROR")
-        pass
+    for i in range(int(threadsInput)):
+        os.system(
+            f"title Roblox Account Gen ^|  Threads: {threadsInput}  ^| by TerrificTable55™#5297")
+        thread = threading.Thread(target=gen)
+        thread.start()
+        threads.append(thread)
+    print(f"{w}[{g}={w}] -> Threads Started")
+
+    for thread in threads:
+        thread.join()
+        os.system(
+            "title Roblox Account Gen ^|  Finished  ^| by TerrificTable55™#5297")
+    print(f"[{g}={w}] -> Threads Finished, press [{y}ENTER{w}] to exit")
+    input()
+    exit()
