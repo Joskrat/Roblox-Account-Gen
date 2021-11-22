9e2199570a6c3fd179f38159368a492b98668753
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2021-11-22 07:40:18 +0100

    changed the run.bat file

--- run.bat
+++ run.bat
@@ -1,5 +1,14 @@
-@echo off
+@echo off & cls
 title Installing Requirements
 python -m pip install colorama requests wget selenium exrex typing maxminddb ipaddress loguru faker pypresence PySocks psutil bs4 tqdm
+cls
+
+type batMenu.py
+set /p action= %cd% $
+if '%action%' == "y" goto start
+if '%action%' == "n" exit
+
+:start
+@echo off & cls
 python main.py
-pause
+exit
