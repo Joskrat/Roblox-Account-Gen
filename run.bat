bff78e14bfcf7d9212bbd9e4de5f64123891db0d
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2021-11-22 07:46:59 +0100

    removed dumb stuff

--- run.bat
+++ run.bat
@@ -1,14 +1,5 @@
 @echo off & cls
 title Installing Requirements
 python -m pip install colorama requests wget selenium exrex typing maxminddb ipaddress loguru faker pypresence PySocks psutil bs4 tqdm
-cls
-
-type batMenu.py
-set /p action= %cd% $
-if '%action%' == "y" goto start
-if '%action%' == "n" exit
-
-:start
-@echo off & cls
 python main.py
 exit
